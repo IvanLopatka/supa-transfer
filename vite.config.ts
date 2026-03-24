ce698177e8feb864d77524a8df2c0c9cc7aef00f
Lopatka Ivan <lopatkaivan630@gmail.com>
2026-03-24 08:40:28 +0200

    feat: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,36 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
   plugins: [
-    react()
+    react(),
+    dts({
+      insertTypesEntry: true,
+      include: ['src'],
+    })
   ],
   build: {
     lib: {
+
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'MySupabaseUtils',
       fileName: (format) => `my-supabase-utils.${format}.js`,
+      formats: ['es', 'umd'],
     },
     rollupOptions: {
+      //libraries that will not be included in the bundle
       external: ['react', 'react-dom', '@supabase/supabase-js'],
       output: {
         globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
           '@supabase/supabase-js': 'Supabase',
         },
       },
     },
+
+    minify: 'esbuild',
   },
 });
